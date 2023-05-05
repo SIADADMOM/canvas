7324b0944ac3c834d70f1e9787c021ff2fe24a73
OUSUK OH <>
2023-05-05 17:19:31 +0900

    add delete and. rase function

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+
 const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas"); 
 const ctx = canvas.getContext("2d"); // 캔버스에 그림을 그리는 것
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value; // 선의 굵기 정해줌
@@ -47,19 +54,29 @@ function onColorClick(event) {
 
 function onModeClick(event) {
   if(isFilling) {
-    isFilling = false
-    modeBtn.innerText = "Fill"
+    isFilling = false;
+    modeBtn.innerText = "Fill";
   } else {
-    isFilling = true
-    modeBtn.innerText = "Draw"
+    isFilling = true;
+    modeBtn.innerText = "Draw";
   }
-  console.log(event);
 }
 
 function onCanvasClick() {
   if(isFilling) {
-    ctx.fillRect(0, 0, 800, 800);
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   }
+}
+
+function onDestroyClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "Fill"
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -75,3 +92,5 @@ color.addEventListener("change", onColorChange);
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
 
 modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
